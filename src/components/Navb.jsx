548aafea290ddc9f5c7e3b606183af3cb7472ca1
Oswaldo <salasoswaldoa@gmail.com>
2022-01-05 14:52:23 -0400

    se agrego carrusel

--- src/components/Navb.jsx
+++ src/components/Navb.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { Navbar, Container,Nav ,NavDropdown} from 'react-bootstrap'
+import image1 from '../image/0f533bed314299722927951166b27447.jpg'
+import image2 from '../image/1.jpeg'
+import image3 from '../image/1e551298af78c13c368f26f37e928261.jpg'
+import image4 from '../image/2e9e5411-6f8b-47c4-b572-b8a66bb824d4.jpeg'
+
+
+
 
 const Navb = () => {
     return (
@@ -30,7 +37,7 @@ const Navb = () => {
             </Container>
         </Navbar>
 
-        
+
         
 
         <nav className="navbar navbar-expand-lg navbar-light bg-light mt-5">
@@ -72,7 +79,45 @@ const Navb = () => {
 
 
 
-
+     
+<div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={image1} class="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src={image2} className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src={image3} className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
 
 
       </div>
